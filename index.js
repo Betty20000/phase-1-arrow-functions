13b87f0fd006ada31abcad5cd5442a6f6d288349
Beatrice Gachigi <beatricegachigi@gmail.com>
2024-03-30 23:40:51 +0300

    arrowfnc

---
+++ index.js
@@ -0,0 +1,30 @@
+// function foo(){
+//     return 'bar';
+// }
+// const nums = [1,2,3];
+// const squares = nums.map(x => x ** 2);
+// squares;
+// console.log(squares);
+// nums;
+
+
+//assignment1
+// a function expression called divide
+const divide = (num1,num2) =>{
+    return num1 / num2;
+}
+console.log(divide(2000,100));
+
+// square arrow function takes one parameter and multiplies it times itself
+const square = (x)=>(x**2);
+// return square(10);
+console.log(square(5));
+
+//arrow function called add
+const add = (value1,value2)=>(value1 + value2);
+console.log(add(200,200));
+
+
+
+ 
+
